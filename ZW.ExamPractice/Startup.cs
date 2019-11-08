873e26fb37c75722f8febf4c4f21209bb91fcf37
Wei Zhang <zhang_wei@star-quest.com>
2019-11-08 14:38:19 +0800

    remove AngularCLI Server

--- ZW.ExamPractice/Startup.cs
+++ ZW.ExamPractice/Startup.cs
@@ -63,7 +63,8 @@ namespace ZW.ExamPractice
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    //  spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
